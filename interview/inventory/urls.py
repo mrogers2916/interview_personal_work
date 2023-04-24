e55fe5a19e5d2255908ae2cf350221b459790fe6
mrogers2916 <mrogers2916@gmail.com>
2023-04-24 11:46:56 -0700

    initialize

---
+++ interview/inventory/urls.py
@@ -0,0 +1,20 @@
+
+from django.urls import path
+from interview.inventory.views import InventoryLanguageListCreateView, InventoryLanguageRetrieveUpdateDestroyView, InventoryListCreateView, InventoryRetrieveUpdateDestroyView, InventoryTagListCreateView, InventoryTagRetrieveUpdateDestroyView, InventoryTypeListCreateView, InventoryTypeRetrieveUpdateDestroyView, InventoryItemsCreatedCertainDay, InventoryItemsDeactivateOrderView, InventoryItemsDateRange
+from interview.order.views import OrderListCreateView, OrderTagListCreateView
+
+
+urlpatterns = [
+    path('<int:id>/', InventoryRetrieveUpdateDestroyView.as_view(), name='inventory-detail'),
+    path('languages/<int:id>/', InventoryLanguageRetrieveUpdateDestroyView.as_view(), name='inventory-languages-detail'),
+    path('tags/<int:id>/', InventoryTagRetrieveUpdateDestroyView.as_view(), name='inventory-tags-detail'),
+    path('types/<int:id>/', InventoryTypeRetrieveUpdateDestroyView.as_view(), name='inventory-types-detail'),
+    path('languages/', InventoryLanguageListCreateView.as_view(), name='inventory-languages-list'),
+    path('tags/', InventoryTagListCreateView.as_view(), name='inventory-tags-list'),
+    path('types/', InventoryTypeListCreateView.as_view(), name='inventory-types-list'),
+    path('', InventoryListCreateView.as_view(), name='inventory-list'),
+    path('createdAfter/<str:date>/', InventoryItemsCreatedCertainDay.as_view(), name='inventory-date-created-list'),
+    path('deactivate/<int:id>/', InventoryItemsDeactivateOrderView.as_view(), name='inventory-items-deactivate-order-view'),
+    path('deactivate/<str:date1>/<str:date2>/', InventoryItemsDateRange.as_view(), name='inventory-items-date-range'),
+    
+]
